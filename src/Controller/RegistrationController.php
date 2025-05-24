4fbd44d50ad9b5112f0b8e97a692fc19082be0ad
jlinares <julien.linares@etudiant.univ-lr.fr>
2025-05-24 11:18:35 +0200

    bug patch & content

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -51,7 +51,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_loved');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
